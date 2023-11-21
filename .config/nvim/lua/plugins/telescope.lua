e480cb961511d75934766948c1c0467f135db886
Andrew781123 <leechihoandrew1123@gmail.com>
2023-11-22 00:07:04 +0800

    lazyvim config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -16,4 +16,19 @@ return {
       },
     }
   end,
+  keys = function()
+    local Util = require("lazyvim.util")
+
+    return {
+      { "<leader>fs", Util.telescope("live_grep"), desc = "Grep (root dir)" },
+      { "<leader>rff", "<cmd>Telescope resume<cr>", desc = "Resume" },
+      { "<leader>ff", Util.telescope("files"), desc = "Find Files (root dir)" },
+      { "<leader>fw", Util.telescope("grep_string", { word_match = "-w" }), desc = "Word (root dir)" },
+      -- git
+      { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
+      { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "status" },
+      -- others
+      { "<leader>uC", Util.telescope("colorscheme", { enable_preview = true }), desc = "Colorscheme with preview" },
+    }
+  end,
 }
