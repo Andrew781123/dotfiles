6e37896d071a370afb11e7e06193d83a89af93da
Andrew781123 <leechihoandrew1123@gmail.com>
2023-11-29 00:27:09 +0800

    2911

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,23 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    colorscheme = "catppuccin",
+  {
+    "rose-pine/neovim",
+    lazy = true,
+    name = "rose-pine",
+  },
+  { "ellisonleao/gruvbox.nvim", lazy = true },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      no_bold = true,
+      no_italic = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
   },
 }
