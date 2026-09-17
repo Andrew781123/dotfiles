f861fec4eb5f0d86e2e2fa5cec1316f91189bdd5
Andrew781123 <leechihoandrew1123@gmail.com>
2026-09-17 12:59:41 +0800

    170926

--- setup.sh
+++ setup.sh
@@ -41,6 +41,7 @@ declare -A symlinks=(
   ["$DOTFILES_DIR/tmux"]="$HOME/.tmux"
   ["$DOTFILES_DIR/tmux-sessionizer.conf"]="$HOME/.config/tmux-sessionizer/tmux-sessionizer.conf"
   ["$DOTFILES_DIR/pi/agent/settings.json"]="$HOME/.pi/agent/settings.json"
+  ["$DOTFILES_DIR/pi/agent/i-have-adhd.json"]="$HOME/.pi/agent/i-have-adhd.json"
 )
 
 # Loop through the associative array to create directories and symlinks
